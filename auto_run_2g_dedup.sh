a39597e791014f9b6972d1e2ad4cdbc44f97dcef
youzhang <youzhang@example.com>
2020-04-13 03:45:03 +0800

    4.14

---
+++ auto_run_2g_dedup.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+if [ $# -ne 2 ]; then
+	echo "error"
+fi
+
+echo $1 $2
+
+./obtain_inter_data $1 $2
+echo ""
+
+./restore_identified_file $1 $2
+echo ""
+
+./restore_migriated_file $1 $2
+echo ""
+
+mv $1/new.meta $1/recipes/bv0.meta 
+mv $1/new.recipe $1/recipes/bv0.recipe
+
+echo ""
+echo ""
+echo ""
+echo ""
+
+./obtain_inter_data $2 $1
+echo ""
+
+./restore_identified_file $2 $1
+echo ""
+
+./restore_migriated_file $2 $1
+echo ""
+
+mv $2/new.meta $2/recipes/bv0.meta 
+mv $2/new.recipe $2/recipes/bv0.recipe
